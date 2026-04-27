5639c4d4f134da213436ef4ef848c44b99ecb6b9
AnDu_MC <101265360+andumc@users.noreply.github.com>
2026-04-27 21:00:34 +0200

    Use venv install for agent node

--- WOX-Dashboard-V2/scripts/install_debian.sh
+++ WOX-Dashboard-V2/scripts/install_debian.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-sudo apt update && sudo apt install -y python3 python3-pip
-pip3 install -r requirements.txt
-python3 app.py
+set -e
+sudo apt update
+sudo apt install -y python3 python3-pip python3-venv
+python3 -m venv venv
+source venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+python app.py
