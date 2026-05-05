8ec6f9b5a2735fbb8c2e4e50a5b909b680e6f3d7
AnDu_MC <101265360+andumc@users.noreply.github.com>
2026-05-05 19:16:25 +0200

    Delete WOX-Dashboard-V2 directory
    just a test, not needed anymore

--- WOX-Dashboard-V2/scripts/install_debian.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-sudo apt update
-sudo apt install -y python3 python3-pip python3-venv
-python3 -m venv venv
-source venv/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
-python app.py
